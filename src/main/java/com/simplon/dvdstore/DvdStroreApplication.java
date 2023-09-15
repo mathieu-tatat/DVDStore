e2abf13566ab3b0231cc5bf9d2fb040e30e25257
mathieu-tatat <mathieu.tatat@laplateforme.io>
2023-09-15 12:42:49 +0200

    indent code

--- src/main/java/com/simplon/dvdstore/DvdStroreApplication.java
+++ src/main/java/com/simplon/dvdstore/DvdStroreApplication.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// commentaire
 
 @SpringBootApplication
 public class DvdStroreApplication{
